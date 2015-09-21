168a584550f57b2d2ba6a7f6c73e53577b9beb27
Björn Dahlgren <bjodah@gmail.com>
2015-09-21 15:35:11 +0200

    More explicit naming: f -> rhs, j -> jac

--- pygslodeiv2/_gslodeiv2_numpy.pyx
+++ pygslodeiv2/_gslodeiv2_numpy.pyx
@@ -62,20 +62,21 @@ step_type_indices = ['rk2', 'rk4', 'rkf45', 'rkck', 'rk8pd', 'rk1imp',
                      'rk2imp', 'rk4imp', 'bsimp', 'msadams', 'msbdf']
 requires_jac = ('rk1imp', 'rk2imp', 'rk4imp', 'bsimp', 'msbdf')
 
-def adaptive(f, j, y0, x0, xend, dx0, atol, rtol, method='bsimp'):
+def adaptive(rhs, jac, y0, x0, xend, dx0, atol, rtol, method='bsimp'):
     cdef size_t nsteps
-    if method in requires_jac and f is None:
+    if method in requires_jac and jac is None:
         raise ValueError("Method requires explicit jacobian callback")
-    integr = GslOdeiv2(f, j, len(y0))
-    nsteps = integr.adaptive(np.asarray(y0, dtype=np.float64), x0, xend, dx0, atol, rtol,
+    integr = GslOdeiv2(rhs, jac, len(y0))
+    nsteps = integr.adaptive(np.asarray(y0, dtype=np.float64),
+                             x0, xend, dx0, atol, rtol,
                              step_type_indices.index(method))
     return integr.get_xout(nsteps), integr.get_yout(nsteps)
 
 
-def predefined(f, j, y0, xout, dx0, atol, rtol, method='bsimp'):
-    if method in requires_jac and f is None:
+def predefined(rhs, jac, y0, xout, dx0, atol, rtol, method='bsimp'):
+    if method in requires_jac and jac is None:
         raise ValueError("Method requires explicit jacobian callback")
-    integr = GslOdeiv2(f, j, len(y0))
+    integr = GslOdeiv2(rhs, jac, len(y0))
     return integr.predefined(np.asarray(y0, dtype=np.float64),
                              np.asarray(xout, dtype=np.float64),
                              dx0, atol, rtol, step_type_indices.index(method))
