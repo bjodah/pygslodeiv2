644d445d7cceb23ad4a9a9dfd9cf63e284fc19e6
Björn Dahlgren <bjodah@gmail.com>
2016-10-13 16:47:39 +0200

    Handle exceptions in parallel region

--- pygslodeiv2/include/gsl_odeiv2_anyode_parallel.hpp
+++ pygslodeiv2/include/gsl_odeiv2_anyode_parallel.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "anyode/anyode_parallel.hpp"
 #include "gsl_odeiv2_anyode.hpp"
 
 namespace gsl_odeiv2_anyode_parallel {
@@ -28,13 +29,19 @@ namespace gsl_odeiv2_anyode_parallel {
         const int nsys = odesys.size();
         auto results = std::vector<sa_t>(nsys);
 
+        anyode_parallel::ThreadException te;
         #pragma omp parallel for
         for (int idx=0; idx<nsys; ++idx){
-            results[idx] = simple_adaptive<OdeSys>(
-                odesys[idx], atol, rtol, styp, y0 + idx*ny, t0[idx], tend[idx],
-                mxsteps, dx0, dx_min, dx_max);
-
+            sa_t local_result;
+            te.run([&]{
+                local_result = simple_adaptive<OdeSys>(
+                    odesys[idx], atol, rtol, styp, y0 + idx*ny, t0[idx], tend[idx],
+                    mxsteps, dx0, dx_min, dx_max);
+            });
+            results[idx] = local_result;
         }
+        te.rethrow();
+
         return results;
     }
 
@@ -56,12 +63,16 @@ namespace gsl_odeiv2_anyode_parallel {
         const int ny = odesys[0]->get_ny();
         const int nsys = odesys.size();
 
+        anyode_parallel::ThreadException te;
         #pragma omp parallel for
         for (int idx=0; idx<nsys; ++idx){
-            simple_predefined<OdeSys>(odesys[idx], atol, rtol, styp, y0 + idx*ny,
-                                      nout, tout + idx*nout, yout + idx*ny*nout,
-                                      mxsteps, dx0, dx_min, dx_max);
+            te.run([&]{
+                simple_predefined<OdeSys>(odesys[idx], atol, rtol, styp, y0 + idx*ny,
+                                          nout, tout + idx*nout, yout + idx*ny*nout,
+                                          mxsteps, dx0, dx_min, dx_max);
+            });
         }
+        te.rethrow();
     }
 
 }
