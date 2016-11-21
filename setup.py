175e6e59fba187b550f43bace25050cbf2d45624
Björn Dahlgren <bjodah@gmail.com>
2016-11-21 16:11:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@
 
 import io
 import os
+import re
 import pprint
 import shutil
+import subprocess
 import sys
 import warnings
 
@@ -75,6 +77,15 @@ if len(RELEASE_VERSION) > 1:
 else:  # set `__version__` from _release.py:
     TAGGED_RELEASE = False
     exec(open(release_py_path).read())
+    if __version__.endswith('git'):
+        try:
+            _git_version = subprocess.check_output(['git', 'describe']).rstrip().decode('utf-8')
+        except subprocess.CalledProcessError:
+            warnings.warn("A git-archive is being installed - version information incomplete.")
+        else:
+            if 'develop' not in sys.argv:
+                warnings.warn("Using git to derive version: dev-branches may compete.")
+                __version__ = re.sub('v([0-9.]+)-(\d+)-(\w+)', r'\1.post\2+\3', _git_version)  # .dev < '' < .post
 
 classifiers = [
     "Development Status :: 4 - Beta",
