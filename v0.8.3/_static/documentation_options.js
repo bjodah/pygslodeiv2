754133b747d13d0d66152938cc094c360746dd2c
Björn Dahlgren <bjodah@gmail.com>
2018-04-30 06:23:27 +0200

    Release docs for v0.8.3

---
+++ v0.8.3/_static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: 'v0.8.3.dirty',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
