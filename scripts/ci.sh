af604f8e21243f95b834bec26b875f7df504ae9f
Björn Dahlgren <bjodah@gmail.com>
2020-03-05 17:20:15 +0100

    Bump CI image (#37)
    * Bump CI image
    
    * New CI image
    
    * relative path fix

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -5,19 +5,19 @@ if [[ "$CI_BRANCH" =~ ^v[0-9]+.[0-9]?* ]]; then
     echo ${CI_BRANCH} | tail -c +2 > __conda_version__.txt
 fi
 
+cd tests/; make CXX=clang++-10 EXTRA_FLAGS=-fsanitize=address; make clean; cd -
+cd tests/; make CXX=clang++-10 EXTRA_FLAGS=-fsanitize=undefined; make clean; cd -
+
+export CC=gcc-10
+export CXX=g++-10
+
 cd tests/; make EXTRA_FLAGS=-D_GLIBCXX_DEBUG; make clean; cd -
 cd tests/; make EXTRA_FLAGS=-DNDEBUG; make clean; cd -
-cd tests/; make CXX=clang++-6.0 EXTRA_FLAGS=-fsanitize=address; make clean; cd -
-cd tests/; make CXX=clang++-6.0 EXTRA_FLAGS=-fsanitize=undefined; make clean; cd -
 
+PKG_VERSION=$(python3 setup.py --version)
 python3 setup.py sdist
-(cd dist/; python3 -m pip install $PKG_NAME-$(python3 ../setup.py --version).tar.gz)
+(cd dist/; python3 -m pip install $PKG_NAME-$PKG_VERSION.tar.gz)
 (cd /; python3 -m pytest --pyargs $PKG_NAME)
 python3 -m pip install --user -e .[all]
 PYTHONPATH=$(pwd) PYTHON=python3 ./scripts/run_tests.sh --cov $PKG_NAME --cov-report html
 ./scripts/coverage_badge.py htmlcov/ htmlcov/coverage.svg
-
-# Make sure repo is pip installable from git-archive zip
-git archive -o /tmp/$PKG_NAME.zip HEAD
-python3 -m pip install --force-reinstall /tmp/$PKG_NAME.zip
-(cd /; python3 -c "from ${PKG_NAME} import get_include as gi; import os; assert 'gsl_odeiv2_cxx.pxd' in os.listdir(gi())")
