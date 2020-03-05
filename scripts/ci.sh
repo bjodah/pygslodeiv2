109b6a58f4d7a4545ba679299ae5744eb4e0f0ec
Björn Ingvar Dahlgren <bjodah@gmail.com>
2020-03-05 16:22:14 +0100

    New CI image

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -5,10 +5,14 @@ if [[ "$CI_BRANCH" =~ ^v[0-9]+.[0-9]?* ]]; then
     echo ${CI_BRANCH} | tail -c +2 > __conda_version__.txt
 fi
 
+cd tests/; make CXX=clang++-10 EXTRA_FLAGS=-fsanitize=address; make clean; cd -
+cd tests/; make CXX=clang++-10 EXTRA_FLAGS=-fsanitize=undefined; make clean; cd -
+
+export CC=gcc-10
+export CXX=g++-10
+
 cd tests/; make EXTRA_FLAGS=-D_GLIBCXX_DEBUG; make clean; cd -
 cd tests/; make EXTRA_FLAGS=-DNDEBUG; make clean; cd -
-cd tests/; make CXX=clang++-6.0 EXTRA_FLAGS=-fsanitize=address; make clean; cd -
-cd tests/; make CXX=clang++-6.0 EXTRA_FLAGS=-fsanitize=undefined; make clean; cd -
 
 python3 setup.py sdist
 (cd dist/; python3 -m pip install $PKG_NAME-$(python3 ../setup.py --version).tar.gz)
@@ -16,8 +20,3 @@ python3 setup.py sdist
 python3 -m pip install --user -e .[all]
 PYTHONPATH=$(pwd) PYTHON=python3 ./scripts/run_tests.sh --cov $PKG_NAME --cov-report html
 ./scripts/coverage_badge.py htmlcov/ htmlcov/coverage.svg
-
-# Make sure repo is pip installable from git-archive zip
-git archive -o /tmp/$PKG_NAME.zip HEAD
-python3 -m pip install --force-reinstall /tmp/$PKG_NAME.zip
-(cd /; python3 -c "from ${PKG_NAME} import get_include as gi; import os; assert 'gsl_odeiv2_cxx.pxd' in os.listdir(gi())")
